2498ad0ff1b7c898e86bd66f84b58d73b96384e7
Grant Norwood <gkn@grantnorwood.com>
2022-12-04 10:54:41 -0600

    Add some SEO stuff (#31)
    * feat:  Add some SEO stuff.
    
    * feat:  Add twitter and open graph meta tags.

--- app/root.tsx
+++ app/root.tsx
@@ -16,10 +16,29 @@ export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
 };
 
+const defaultTitle =
+  "Remix Austin 💿 A community & monthly Meetup event for Remix developers";
+const defaultDescription =
+  "We are the premiere Remix community for developers in Austin, TX, and we stream remotely all over the world!";
+const baseUrl = "https://remixaustin.com/";
+const logoUrl = "https://remixaustin.com/img/remix-logo-rainbow.jpg";
+
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Austin 💿",
+  title: defaultTitle,
+  description: defaultDescription,
   viewport: "width=device-width,initial-scale=1",
+  "twitter:card": "summary_large_image",
+  "twitter:title": defaultTitle,
+  "twitter:description": defaultDescription,
+  "twitter:image": logoUrl,
+  "twitter:image:alt": "Remix Austin logo",
+  "og:type": "website",
+  "og:title": defaultTitle,
+  "og:site_name": "Remix Austin 💿",
+  "og:description": defaultDescription,
+  "og:url": baseUrl,
+  "og:image": logoUrl,
 });
 
 export default function App() {
