f8fe3b73e8e217b3b3f0782cd13502aed0ecc3e1
Colby M. White <colbywhite@users.noreply.github.com>
2022-12-08 23:27:20 -0600

    fix: ensure e2e cmd works locally

--- playwright.config.ts
+++ playwright.config.ts
@@ -106,10 +106,10 @@ const config: PlaywrightTestConfig = {
   outputDir: "playwright-results/",
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+  webServer: {
+    command: "npm run dev",
+    port: 3000,
+  },
 };
 
 export default config;
