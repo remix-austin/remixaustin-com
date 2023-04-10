fcfca767e2ff62470831d7f14614ee6f98db866b
Matt Hernandez <matt.isaiah.hernandez@gmail.com>
2023-04-10 12:23:10 -0500

    Update prettier ignore

--- blog/posts/components/AuthorFooter.tsx
+++ blog/posts/components/AuthorFooter.tsx
@@ -1,19 +1,13 @@
-import { type ReactNode } from "react";
+import { type PropsWithChildren } from "react";
 
-/**
- * Using `contents` here instead of `children` because of a weird bug
- * with nesting children in `mdx-bundler`. Our prettier auto-format
- * kept making it bug out too. Super weird stuff.
- */
 export function AuthorFooter({
   imageSrc,
   alt,
-  contents,
+  children,
 }: {
   imageSrc: string;
   alt: string;
-  contents: ReactNode;
-}) {
+} & PropsWithChildren) {
   return (
     <div className="flex flex-row items-center">
       <img
@@ -21,7 +15,7 @@ export function AuthorFooter({
         src={imageSrc}
         alt={alt}
       />
-      <p className="ml-4 flex-1">{contents}</p>
+      <div className="ml-4 flex-1">{children}</div>
     </div>
   );
 }
