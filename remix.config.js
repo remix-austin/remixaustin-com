3b73fe65e010abe5d20e2dfb2637ea01d02def65
TC Schiller <tc@tcschiller.com>
2023-05-23 14:12:36 -0500

    Tweak Remix config for v2_errorBoundary, v2_normalizeFormMethod, and serverModuleFormat (cjs for now)

--- remix.config.js
+++ remix.config.js
@@ -3,12 +3,17 @@
  */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
+  future: {
+    v2_errorBoundary: true,
+    v2_normalizeFormMethod: true,
+  },
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
   serverDependenciesToBundle: [
     "remark-mdx-images",
     /^unist-util-.*/,
     /^mdx-bundler.*/,
   ],
+  serverModuleFormat: "cjs",
   watchPaths: [
     "./blog/*.ts",
     "./public/posts",
