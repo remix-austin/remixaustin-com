c60936e1b946e4bea3ded6e806575e76e806a375
Matt Hernandez <matt.isaiah.hernandez@gmail.com>
2023-05-24 13:59:03 -0500

    Use vitest to set system time for blog cache headers tests

--- app/routes/blog.$slug.test.tsx
+++ app/routes/blog.$slug.test.tsx
@@ -13,8 +13,7 @@ const TEST_URL = "https://test.io";
 const SLUG = "slug";
 const TEST_GET_BLOG_POST_URL = `${TEST_URL}/resource/get-blog-post/${SLUG}`;
 const TEST_POST_URL = `${TEST_URL}/${SLUG}`;
-const ONE_MINUTE_MS = 1000 * 60;
-const ONE_DAY_MS = ONE_MINUTE_MS * 60 * 24;
+const ONE_DAY_MS = 1000 * 60 * 60 * 24;
 
 describe("/blog/$slug", () => {
   it("should bundle mdx", async () => {
@@ -42,11 +41,20 @@ Hello, world.
     expect(code).toContain("Hello, world.");
   });
 
-  it("should have a Cache-Control header with a max-age of 8 hours if post is less than 4 days old", async () => {
-    const today = Date.now();
-    const fourDaysAgoMs = today - ONE_DAY_MS * 4 + ONE_MINUTE_MS; // 1 minute "safety buffer"
-    const publishDate = new Date(fourDaysAgoMs).toISOString();
-    const testMdx = `---
+  describe("Cache-Control headers", () => {
+    beforeEach(() => {
+      vi.useFakeTimers();
+    });
+
+    afterEach(() => {
+      vi.useRealTimers();
+    });
+
+    it("should have a Cache-Control header with a max-age of 8 hours if post is less than 4 days old", async () => {
+      vi.setSystemTime(Date.now());
+      const fourDaysAgoMs = Date.now() - ONE_DAY_MS * 4 + 1; // Just under 4 days old
+      const publishDate = new Date(fourDaysAgoMs).toISOString();
+      const testMdx = `---
 title: Title
 date: ${publishDate}
 ---
@@ -54,21 +62,21 @@ date: ${publishDate}
 
 Hello, world.
 `;
-    mockUrlResponse(TEST_GET_BLOG_POST_URL, {
-      json: await bundleMdx(testMdx, SLUG, POSTS_BUILD_DIR),
+      mockUrlResponse(TEST_GET_BLOG_POST_URL, {
+        json: await bundleMdx(testMdx, SLUG, POSTS_BUILD_DIR),
+      });
+      const args = urlToLoaderArgs(TEST_POST_URL, { path: { slug: SLUG } });
+      const response = await loader(args);
+      const cacheControlHeader = response.headers.get("Cache-Control");
+      expect(cacheControlHeader).not.toBeNull();
+      expect(cacheControlHeader).toBe(SHORT_CACHE_MAX_AGE);
     });
-    const args = urlToLoaderArgs(TEST_POST_URL, { path: { slug: SLUG } });
-    const response = await loader(args);
-    const cacheControlHeader = response.headers.get("Cache-Control");
-    expect(cacheControlHeader).not.toBeNull();
-    expect(cacheControlHeader).toBe(SHORT_CACHE_MAX_AGE);
-  });
 
-  it("should have a Cache-Control header with a max-age of 1 week if post is more than 4 days old", async () => {
-    const today = Date.now();
-    const fourDaysAgoMs = today - ONE_DAY_MS * 4 - ONE_MINUTE_MS; // 1 minute "safety buffer"
-    const publishDate = new Date(fourDaysAgoMs).toISOString();
-    const testMdx = `---
+    it("should have a Cache-Control header with a max-age of 1 week if post is 4 days old or older", async () => {
+      vi.setSystemTime(Date.now());
+      const fourDaysAgoMs = Date.now() - ONE_DAY_MS * 4;
+      const publishDate = new Date(fourDaysAgoMs).toISOString();
+      const testMdx = `---
 title: Title
 date: ${publishDate}
 ---
@@ -76,13 +84,14 @@ date: ${publishDate}
 
 Hello, world.
 `;
-    mockUrlResponse(TEST_GET_BLOG_POST_URL, {
-      json: await bundleMdx(testMdx, SLUG, POSTS_BUILD_DIR),
+      mockUrlResponse(TEST_GET_BLOG_POST_URL, {
+        json: await bundleMdx(testMdx, SLUG, POSTS_BUILD_DIR),
+      });
+      const args = urlToLoaderArgs(TEST_POST_URL, { path: { slug: SLUG } });
+      const response = await loader(args);
+      const cacheControlHeader = response.headers.get("Cache-Control");
+      expect(cacheControlHeader).not.toBeNull();
+      expect(cacheControlHeader).toBe(LONG_CACHE_MAX_AGE);
     });
-    const args = urlToLoaderArgs(TEST_POST_URL, { path: { slug: SLUG } });
-    const response = await loader(args);
-    const cacheControlHeader = response.headers.get("Cache-Control");
-    expect(cacheControlHeader).not.toBeNull();
-    expect(cacheControlHeader).toBe(LONG_CACHE_MAX_AGE);
   });
 });
